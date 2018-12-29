d9beec45ff2f80dc12b23bd70c510aa6d4401b27
Erik Larson <erik@welarson.com>
2018-12-29 15:27:20 -0600

    Fix data directory name

--- src/renderer/store/modules/config.js
+++ src/renderer/store/modules/config.js
@@ -2,7 +2,7 @@ import { remote } from 'electron';
 import path from 'path';
 
 const state = {
-   poddir: path.join(remote.app.getPath('home'), 'podpupper'),
+   poddir: path.join(remote.app.getPath('home'), 'podpup'),
    concurrentDownloads: 3,
    concurrentRefreshes: 3
 };
